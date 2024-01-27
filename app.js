47a33aa45fcf3fe970089eb850c069ce83faef32
mk4562 <157869047+mk4562@users.noreply.github.com>
2024-01-27 00:31:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+fetch('data.js')
+  .then(response => response.json())
+  .then(data => {
+    const links = data.links;
+    links.forEach(link => {
+      console.log(`Name: ${link.name_url}, URL: ${link.url}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error al cargar el archivo JSON:', error);
+  });
