9e4cbb53fdcd13006692ef7fd8067395006f87fb
mk4562 <157869047+mk4562@users.noreply.github.com>
2024-01-27 01:13:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-fetch('data.js')
-  .then(response => response.json())
-  .then(data => {
-    const links = data.links;
-    links.forEach(link => {
-      console.log(`Name: ${link.name_url}, URL: ${link.url}`);
-    });
-  })
-  .catch(error => {
-    console.error('Error al cargar el archivo JSON:', error);
-  });
+const jsonUrl = 'https://mk4562.github.io/mk500M/data.json';
+
+async function redirectToRandomLink() {
+    try {
+        const response = await fetch(jsonUrl);
+        const jsonData = await response.json();
+        const randomIndex = Math.floor(Math.random() * jsonData.length);
+        const randomUrl = jsonData[randomIndex].url;
+        window.location.href = randomUrl;
+    } catch (error) {
+        console.error('Error al obtener o procesar el JSON:', error);
+    }
+}
+
+redirectToRandomLink();
